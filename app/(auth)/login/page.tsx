8dde2848b6ddac6b2b235fed8ab7bb78ab6076cf
Jo Cohen <jojocohen57@gmail.com>
2024-12-23 16:17:05 +0200

    lessons 21-24

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,7 +1,24 @@
+"use client"
+
+import AuthForm from "../Authform"
+
 export default function Login() {
-    return (
-      <main>
-        <h2 className="text-center">Login</h2>
-      </main>
-    )
-  }
+
+   const handleSubmit = async (
+      e: React.FormEvent<HTMLFormElement>, // Typing for e
+      email: string,
+      password: string
+    ) => {
+      e.preventDefault();
+  
+      console.log(email, password, "login");
+    };
+
+  return (
+    <main>
+      <h2 className="text-center">Login</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
+    </main>
+  )
+}
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,44 @@
+"use client"
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+
+// components
+import AuthForm from "../Authform"
+
 export default function Signup() {
-    return (
-      <main>
-        <h2 className="text-center">Sign up</h2>
-      </main>
-    )
+  const router = useRouter()
+  const [error, setError] = useState('')
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>, email: string, password: string) => {
+    e.preventDefault()
+    setError('')
+
+    const supabase = createClientComponentClient()
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`
+      }
+    })
+    if (error) {
+      setError(error.message)
+    }
+    if (!error) {
+      router.push('/verify')
+    } 
   }
+
+  return (
+    <main>
+      <h2 className="text-center">Sign up</h2>
+
+      <AuthForm handleSubmit={handleSubmit} />
+
+      {error && (
+        <div className="error">{error}</div>
+      )}
+    </main>
+  )
+}
